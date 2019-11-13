8c3d8de78351738cf147db532841a958634ccf05
acamilo <alex.camilo@gmail.com>
2019-11-13 14:51:47 -0500

    make a nojekyll file

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ echo -e "\e[33mBuilding Sphynx Docs.. \e[39mD"
 sphinx-build -nWT -b html source/ build/
 echo -e "\e[33mCopying over static landing page.. \e[39m"
 cp -R landing/* build/
+touch build/.nojekyll
 echo -e "\e[32mDone! \e[39m"
