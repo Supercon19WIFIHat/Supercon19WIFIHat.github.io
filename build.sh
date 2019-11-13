053cb7adf3b2b988c00ef3ff1a927f1a8fffc5fa
acamilo <alex.camilo@gmail.com>
2019-11-13 13:36:21 -0500

    Initial commit of CI web docs

---
+++ build.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+echo -e "\e[33mBuilding Sphynx Docs.. \e[39mD"
+sphinx-build -nWT -b html source/ build/
+echo -e "\e[33mCopying over static landing page.. \e[39m"
+cp -R landing/* build/
+echo -e "\e[32mDone! \e[39m"
