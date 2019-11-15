a3bd77da5f8f08ac44b007b9dc1eeca04d5554a9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-15 02:52:58 +0000

    Deploy Supercon19WIFIHat/Supercon19WIFIHat.github.io to github.com/Supercon19WIFIHat/Supercon19WIFIHat.github.io.git:master

--- searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["docs"],envversion:50,filenames:["docs.rst"],objects:{},objnames:{},objtypes:{},terms:{},titles:["Getting Started"],titleterms:{get:0,section:0,start:0,test:0}})
