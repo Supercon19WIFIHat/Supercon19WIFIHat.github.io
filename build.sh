de16e42ae348f63155aa4942a6cf9da08785e1a1
Alex Camilo <alex.camilo@gmail.com>
2019-11-16 14:03:13 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 echo -e "\e[33mBuilding Sphynx Docs.. \e[39mD"
-sphinx-build -nWT -b html source/ build/
+sphinx-build -nwT -b html source/ build/
 echo -e "\e[33mCopying over static landing page.. \e[39m"
 cp -R landing/* build/
 touch build/.nojekyll
